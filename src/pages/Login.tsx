22526786e7e0b5d34d41571a0cd386c881e5634d
kangsujung <rkdtnwjd1030@naver.com>
2025-05-27 23:55:51 +0900

    ETR-29 Feat: 로그인-회원가입 페이지 연결

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,20 +1,19 @@
-import { FaGoogle } from "react-icons/fa";
-import { SiNaver } from "react-icons/si";
-import { RiKakaoTalkFill } from "react-icons/ri";
 import { IoClose } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 function Login() {
   return(
     <>
 
-        {/* 로그인 모달 */}
         <div className="flex items-center justify-center min-h-screen bg-black-200">
+          {/* 로그인 모달 */}
           <div className="relative bg-white-100 rounded-xl p-20 w-full max-w-lg shadow-xl text-black">
             <button className="absolute top-4 right-4 text-gray-400 hover:text-black">
               <IoClose size={20} />
             </button>
             <h1 className="text-5xl font-black text-center mb-12">DeepTruth</h1>
 
+            {/* ID/PW 입력창 */}
             <input
               type="text"
               placeholder="아이디"
@@ -26,12 +25,15 @@ function Login() {
               className="w-full px-4 py-2 mb-4 border rounded-md focus:outline-none focus:ring focus:border-black"
             />
 
+            {/* ID/PW 버튼 */}
             <button className="w-full bg-green-100 hover:bg-green-500 text-white font-semibold py-2 rounded-md mb-2">
               로그인
             </button>
-            <button className="w-full bg-green-200 hover:bg-green-600 text-black font-semibold py-2 rounded-md mb-4">
-              회원가입
-            </button>
+            <Link to="/signin">
+              <button className="w-full bg-green-200 hover:bg-green-600 text-black font-semibold py-2 rounded-md mb-4">
+                회원가입
+              </button>
+            </Link>
 
             {/* 소셜 로그인 */}
             <div className="max-w-sm w-full mx-auto p-4">  
