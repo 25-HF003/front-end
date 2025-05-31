685adf703bc686c8eec118f4a8c359be1dd6211f
fsdffds <qhals_01@naver.com>
2025-06-01 01:45:36 +0900

    ETR-30 Fix: 파일명 수정으로 인한 import 파일 변경 및 css 변경

--- src/pages/AdversarialNoise.tsx
+++ src/pages/AdversarialNoise.tsx
@@ -1,9 +1,24 @@
-import ImageUploadPage from "../components/ImageUpload/ImageUploadPage";
+import { useState } from "react";
+import FileUploadPage from "../components/Upload/FileUploadPage";
 
 function AdversarialNoise() {
+
+  const [file, setFile] = useState<File | null>(null);
+  const [isSModal, setIsModal] = useState(false);
+
+  const handleNoiseInsert = () => {
+
+  }
+
   return(
     <>
-      <ImageUploadPage />
+      <FileUploadPage 
+        title="이미지"
+        file={file}
+        setFile={setFile}
+        accpet="image/*"
+        onDone={handleNoiseInsert}
+      />
     </>)
 }
 
