135ee75231822443518d1572e2ec78e5594886b7
fsdffds <qhals_01@naver.com>
2025-08-15 16:04:45 +0900

    ETR-72 Feat: 워터마크 탐지 실패 시 파일 초기화 및 로그인 예외 처리 추가

--- src/pages/Watermark/WatermarkDetection.tsx
+++ src/pages/Watermark/WatermarkDetection.tsx
@@ -13,18 +13,40 @@ function WatermarkDetection() {
   const navigate = useNavigate();
 
   const isLoggedIn = useSelector((state: RootState) => !!state.auth.accessToken); 
-  console.log("isLoggedIn", isLoggedIn);
 
   const handleDone = async () => {
-
-    if (!file || !isLoggedIn) return;
+    if (!isLoggedIn) {
+      alert("로그인이 필요합니다.");
+      navigate("/login");
+      return;
+    }
+    
+    if (!file) return;
 
     try {
       const data = await postWatermarkDetection(file);
       navigate("/watermark-report", { state: data });
-    } catch (error) {
-      console.log("실패", error)
-    }
+    } catch (error: any) {
+      if (error.response) {
+      // 예외 처리
+      const { status, message } = error.response.data;
+
+      // 워터마크가 없는 이미지를 탐지했을 때 (임계값 초과)
+      if (status === 404 && message?.startsWith("유사도가 임계값을 넘습니다")) {
+        alert("워터마크가 삽입된 이미지를 넣어주세요!");
+        setFile(null);    // 파일 초기화
+        return;
+      }
+
+      // 다른 에러
+      alert(`에러 발생: ${message ?? '알 수 없는 오류'}`);
+      console.log("실패", status, message);
+      setFile(null);    // 파일 초기화
+      } else {
+      alert("서버와 연결할 수 없습니다.");
+      setFile(null);    // 파일 초기화
+      }
+    } 
   }
 
   return(
